943488c1b0d630fe5c51c34dfb01d5a17efe7be1
nulfrost <khadane.miller@gmail.com>
2023-12-06 16:17:45 -0500

    remove tailwind, install unocss

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+import UnoCSS from "unocss/astro"
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://danethe.dev",
+  site: "https://dane.computer",
   integrations: [
-    tailwind({
-      applyBaseStyles: false,
+    UnoCSS({
+      injectReset: true
     }),
     sitemap(),
   ],

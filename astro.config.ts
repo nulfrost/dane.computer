e034d5d5184d0a2bf09390873990a1d51184f9b2
nulfrost <khadane.miller@gmail.com>
2023-12-06 22:14:16 -0500

    home layout kinda done

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, sharpImageService } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import UnoCSS from "unocss/astro"
+import UnoCSS from "unocss/astro";
 
 export default defineConfig({
   site: "https://dane.computer",
+  image: {
+    service: sharpImageService(),
+  },
   integrations: [
     UnoCSS({
-      injectReset: true
+      injectReset: true,
     }),
     sitemap(),
   ],
--- uno.config.ts
+++ uno.config.ts
@@ -1,4 +1,20 @@
-import { defineConfig } from 'unocss'
+import {
+  defineConfig,
+  presetWebFonts,
+  transformerVariantGroup,
+  presetUno,
+  presetIcons,
+} from "unocss";
 
 export default defineConfig({
-})
+  presets: [
+    presetUno(),
+    presetWebFonts({
+      fonts: {
+        sans: "Asap:100,300,400,600,700",
+      },
+    }),
+    presetIcons(),
+  ],
+  transformers: [transformerVariantGroup()],
+});

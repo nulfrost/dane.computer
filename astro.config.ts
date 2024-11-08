b41af5c527709de82257fb6acfa3730ee82231a8
nulfrost <khadane.miller@gmail.com>
2024-11-07 22:42:37 -0500

    feat: switch to cloudflare

--- astro.config.ts
+++ astro.config.ts
@@ -3,38 +3,36 @@ import sitemap from "@astrojs/sitemap";
 import UnoCSS from "unocss/astro";
 import mdx from "@astrojs/mdx";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://dane.computer",
   experimental: {
     contentLayer: true,
-    contentIntellisense: true,
+    contentIntellisense: true
   },
   image: {
-    service: sharpImageService(),
+    service: sharpImageService()
   },
-  integrations: [
-    UnoCSS({
-      injectReset: true,
-    }),
-    sitemap(),
-    mdx(),
-  ],
+  integrations: [UnoCSS({
+    injectReset: true
+  }), sitemap(), mdx()],
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
       theme: "solarized-light",
       wrap: true,
-      transformers: [
-        {
-          preprocess(code) {
-            if (code.endsWith("\n")) {
-              code = code.slice(0, -1);
-            }
-            return code;
-          },
-        },
-      ],
-    },
+      transformers: [{
+        preprocess(code) {
+          if (code.endsWith("\n")) {
+            code = code.slice(0, -1);
+          }
+          return code;
+        }
+      }]
+    }
   },
+  output: "server",
+  adapter: cloudflare()
 });

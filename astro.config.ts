dcc598ecc2dc64ef1ef19ee9a48e2c891db4fd97
nulfrost <khadane.miller@gmail.com>
2024-11-09 13:35:12 -0500

    chore: more cloudflare stuff

--- astro.config.ts
+++ astro.config.ts
@@ -10,29 +10,39 @@ export default defineConfig({
   site: "https://dane.computer",
   experimental: {
     contentLayer: true,
-    contentIntellisense: true
+    contentIntellisense: true,
   },
   image: {
-    service: sharpImageService()
+    service: sharpImageService(),
   },
-  integrations: [UnoCSS({
-    injectReset: true
-  }), sitemap(), mdx()],
+  integrations: [
+    UnoCSS({
+      injectReset: true,
+    }),
+    sitemap(),
+    mdx(),
+  ],
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
       theme: "solarized-light",
       wrap: true,
-      transformers: [{
-        preprocess(code) {
-          if (code.endsWith("\n")) {
-            code = code.slice(0, -1);
-          }
-          return code;
-        }
-      }]
-    }
+      transformers: [
+        {
+          preprocess(code) {
+            if (code.endsWith("\n")) {
+              code = code.slice(0, -1);
+            }
+            return code;
+          },
+        },
+      ],
+    },
   },
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });

948c63b49d77baf183208d6a26d5e27a7eddf5f3
nulfrost <khadane.miller@gmail.com>
2025-08-07 07:45:37 -0400

    update blog index page

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,19 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 import { leafletStaticLoader } from "@nulfrost/leaflet-loader-astro";
+import { glob } from "astro/loaders";
+
+const blogs = defineCollection({
+	loader: glob({ pattern: "**/*.mdx", base: "./src/data/blog" }),
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		year: z.number(),
+		published_at: z.date(),
+	}),
+});
 
 const documents = defineCollection({
 	loader: leafletStaticLoader({ repo: "did:plc:qttsv4e7pu2jl3ilanfgc3zn" }),
 });
 
-export const collections = { documents };
+export const collections = { documents, blogs };
